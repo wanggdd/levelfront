ef7a44e2c7940915a94e48cdd38261c6220566c2
wangyifeng <wangyifeng@yixia.com>
2019-11-14 12:23:45 +0800

    创建用户详细页面

---
+++ dom/myinfo.php
@@ -0,0 +1,7 @@
+<?php
+include_once $_SERVER['DOCUMENT_ROOT'].'/setting.php';
+include_once $_SERVER['DOCUMENT_ROOT'].'/include/public.php';
+
+$userid = USER_USER_ID;
+$username = USER_USER_NAME;
+$avatar = USER_USER_HEAD_PIC;
